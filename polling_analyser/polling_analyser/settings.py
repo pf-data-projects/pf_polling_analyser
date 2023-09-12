242ad6e6083923e9862f1ee8beaefe3c84c3139b
Jeremy Simons <jeremy.simons@publicfirst.co.uk>
2023-09-12 12:07:20 +0100

    Add custom user profile for authorisation purposes.

--- polling_analyser/polling_analyser/settings.py
+++ polling_analyser/polling_analyser/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
-if os.path.exists("env.py"):
+if os.path.isfile("env.py"):
     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "not_a_secret_for_development_only"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'profiles',
 ]
 
 MIDDLEWARE = [
