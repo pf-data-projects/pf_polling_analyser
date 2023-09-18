9ea68ef06c7ee9a5f78c120389e24870916fd9fb
Jeremy Simons <jeremy.simons@publicfirst.co.uk>
2023-09-18 17:27:34 +0100

    Add workaround for bug - extra questions in the question list

--- queries/table_calculations/calculate_totals.py
+++ queries/table_calculations/calculate_totals.py
@@ -23,19 +23,27 @@ for i in range(len(questions)):
 
 def columns_with_substring(df, substring):
     """
-    Small helper function that returns the column
-    of a dataframe that contains a specified substring.
+    Helper function that returns the column
+    of a dataframe which contains a specified substring.
     """
     return [
         col for col in df.columns if col.split(" : ", 1)[0] == substring
         ]
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Work out the totals for each question
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Work out the totals for each question
 
 for question in question_list:
+    if question['question'] == "Skip / Disqualify Logic":
+        continue
     filtered_df = results[columns_with_substring(results, question['qid'])]
     all_options = question_data.loc[(question_data['question_text'] == 'Option')]
     relevant_options = all_options.loc[
-        (all_options['question_id'] == int(question_list[81]['qid']))
+        (all_options['question_id'] == int(question['qid']))
+    ]
+    options = relevant_options['question_title'].tolist()
+    print(options)
+    second_filtered_df = filtered_df.loc[
+        (filtered_df[f'{question["qid"]} : {question["question"]}'] == options[1])
         ]
-    print(relevant_options)
+
+print("OK")
