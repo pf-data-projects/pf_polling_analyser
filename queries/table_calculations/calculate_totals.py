cffcedd9b9f552a7427b6771802fd12701176ab3
Jeremy Simons <jeremy.simons@publicfirst.co.uk>
2023-09-19 12:25:05 +0100

    clean up calculate_totals file, add comments

--- queries/table_calculations/calculate_totals.py
+++ queries/table_calculations/calculate_totals.py
@@ -4,10 +4,6 @@ from create_blank_table import create_blank_table
 
 results = pd.read_csv('response_data.csv')
 question_data = pd.read_csv('question_data.csv')
-questions_only = question_data.loc[
-    question_data['question_text'] == 'Question'
-]
-question_iterator = results.columns.tolist()
 
 table = create_blank_table()
 questions = table['Answers'].tolist()
@@ -35,24 +31,30 @@ def columns_with_substring(df, substring):
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Work out the totals for each question
 
+# loops through all question options to find
+# number of respondents who answered a certain way
 for question in question_list:
     filtered_df = results[columns_with_substring(results, question['qid'])]
+    # checks that question exists in responses.
     if not filtered_df.empty:
         all_options = question_data.loc[(question_data['question_text'] == 'Option')]
         relevant_options = all_options.loc[
             (all_options['question_id'] == int(question['qid']))
         ]
         options = relevant_options['question_title'].tolist()
-        # print(options)
+        # checks that there are options for the question.
+        # E.G. Age crossbreak question has no options.
         if len(options) > 0:
             for i, option in enumerate(options):
                 second_filtered_df = filtered_df[filtered_df.iloc[:, 0] == options[i]]
-                # print(question['qid'], options[i], len(second_filtered_df.index))
                 position = table[(
                     table['Answers'] == options[i]
                 ) & (
                     table['IDs'] == question['qid']
                 )].index
+                # Checks that this exists in the table.
+                # I think the different indexers for
+                # different loops are not quite the same.
                 if len(position) > 0:
                     position_int = int(position[0])
                     table.iat[position_int, 2] = len(second_filtered_df.index)
