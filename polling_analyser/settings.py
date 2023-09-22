0e416192709bfd133da4cbb9c40046ab5ea6809c
Jeremy Simons <jeremy.simons@publicfirst.co.uk>
2023-09-22 14:50:14 +0100

    Add a view and template to handle frontend and logic

--- polling_analyser/settings.py
+++ polling_analyser/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
     'profiles',
     'queries',
+    'upload',
 ]
 
 SITE_ID = 1
--- upload/views.py
+++ upload/views.py
@@ -1,3 +1,23 @@
+import pandas as pd
 from django.shortcuts import render
+from .forms import CSVUploadForm
 
-# Create your views here.
+def upload_csv(request):
+    """
+    A view that:
+    1. renders the csv upload form
+    2. reads submitted csvs as a pandas dataframe
+    """
+    data = None
+    if request.method == 'POST':
+        form = CSVUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            csv_file = request.FILES['csv_file']
+            data = pd.read_csv(csv_file)
+    else:
+        form = CSVUploadForm()
+
+    return render(request, 'upload.html', {
+        'form': form,
+        'data': data.to_dict(orient='records') if data is not None else None,
+    })
