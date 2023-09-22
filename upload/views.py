e0e327a3291dbc2358726561891fb128e08beceb
Jeremy Simons <jeremy.simons@publicfirst.co.uk>
2023-09-22 16:03:55 +0100

    import python-docx and set up to read survey legend.

--- upload/views.py
+++ upload/views.py
@@ -1,6 +1,18 @@
 import pandas as pd
+from docx import Document
+
 from django.shortcuts import render
 from .forms import CSVUploadForm
+
+def read_word_file(file):
+    """ 
+    Helper function to convert word file into string.
+    """
+    doc = Document(file)
+    result = []
+    for paragraph in doc.paragraphs:
+        result.append(paragraph.text)
+    return '\n'.join(result)
 
 def upload_csv(request):
     """
@@ -8,13 +20,20 @@ def upload_csv(request):
     1. renders the csv upload form
     2. reads submitted csvs as a pandas dataframe
     """
-    data = None
     if request.method == 'POST':
         form = CSVUploadForm(request.POST, request.FILES)
         if form.is_valid():
-            csv_file = request.FILES['csv_file']
-            data = pd.read_excel(csv_file)
+            data_file = request.FILES['data_file']
+            order_file = request.FILES['order_file']
+            survey_legend = request.FILES['survey_legend_file']
+
+            data = pd.read_excel(data_file)
+            order = pd.read_excel(order_file)
+            legend_text = read_word_file(survey_legend)
+
             print(data.head(10))
+            print(order.head(10))
+            print(legend_text)
     else:
         form = CSVUploadForm()
 
