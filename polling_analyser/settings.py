a20acd4ea6243f8bf912e99ee5bb81fba6d97a43
Jeremy Simons <jeremy.simons@publicfirst.co.uk>
2023-10-09 11:32:09 +0100

    temporarily set an insecure fallback secret key for testing on GCP

--- polling_analyser/settings.py
+++ polling_analyser/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.getenv("SECRET_KEY")
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", "fallback_key_01")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if "DEV" in os.environ:
