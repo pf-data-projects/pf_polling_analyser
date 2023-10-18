fc2f36c7d2fe5d0f47c0e66a19bafac04cceae92
Jeremy Simons <jeremy.simons@publicfirst.co.uk>
2023-10-18 12:18:29 +0100

    Add outline of first step of non standard cb logic

---
+++ queries/table_calculations/define_non_standard_cb.py
@@ -0,0 +1,12 @@
+"""
+This file wil take the crossbreak data from the view and
+process it into a list of dictionaries that contain the following
+information about each crossbreak:
+1. column name
+2. question needed to isolate the desired values.
+3. Answer to this question with which to filter results.
+"""
+
+NON_STANDARD_CB = [
+
+]
