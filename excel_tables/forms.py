b9ceeecc317deed0827ee62aab896788f11d2ac9
Jeremy Simons <jeremy.simons@publicfirst.co.uk>
2023-12-22 09:51:31 +0000

    FEAT: update the layout of table scan form.

--- excel_tables/forms.py
+++ excel_tables/forms.py
@@ -23,7 +23,6 @@ class TableUploadForm(forms.Form):
 
     data_file = forms.FileField(
         label='UPLOAD THE TABLE HERE',
-        help_text='Only .xlsx files are accepted.',
         validators=[]
     )
     title = forms.CharField(
@@ -32,12 +31,10 @@ class TableUploadForm(forms.Form):
     )
     dates = forms.CharField(
         label='Please specify the dates in which this survey was in the field.',
-        help_text='e.g. "1st - 7th January 2000"',
         required=True
     )
     start = forms.IntegerField(
         label="Select which question ID you would like the table to start at",
-        help_text="For now, you need to specify the question ID here. It's the number that the question has in the results sheet you download from Alchemer.",
         required=True
     )
     end = forms.IntegerField(
@@ -81,6 +78,5 @@ class TableScanForm(forms.Form):
     """
     data_file = forms.FileField(
         label='Upload the results',
-        help_text="You will then be prompted to enter rebase comments for the questions that need it.",
         required=True
     )
