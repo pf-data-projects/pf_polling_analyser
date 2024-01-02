d285c11180dd68f1a0f6f654d6b6f0ae67913ba9
Jeremy Simons <jeremy.simons@publicfirst.co.uk>
2024-01-02 16:43:13 +0000

    FEAT: add forms and views to report app.

--- report/views.py
+++ report/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+"""
+A module to handle 
+"""
 
-# Create your views here.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Django
+from django.shortcuts import render, redirect
+from django.views import generic
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Internal
+from .models import ReportMessage
+from .forms import ReportForm
+
+
+class CreateReportMessage(generic.CreateView):
+    """
+    A class for handling submitting bug reports.
+    """
+    model = ReportMessage
+    form_class = ReportForm
+    template_name = 'report.html'
+
+    def post(self, request, *args, **kwargs):
+        """
+        submits user's bug report data to db.
+        """
+        form = ReportForm(data=request.POST)
+        if form.is_valid():
+            form.instance.user = request.user
+            form.save()
+            print("Issue report successfully submitted")
+            return redirect('home')
+        else:
+            form = ReportMessage()
+            print("Issue report submission not valid")
+            return redirect('report')
