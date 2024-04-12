608a7bde1cddf8b6f0a2aff1de13da636351b47c
Jeremy Simons <jeremy.simons@publicfirst.co.uk>
2024-04-12 10:32:30 +0100

    FIX: address bug that allows question slippage in rebasing

--- queries/table_calculations/helpers.py
+++ queries/table_calculations/helpers.py
@@ -2,6 +2,9 @@
 Extra functions that help 
 """
 
+import re
+
+
 def col_with_substr(df, substring):
     """
     Helper function that returns the column
@@ -12,6 +15,7 @@ def col_with_substr(df, substring):
     return [
         col for col in df.columns if col.split(": ", 1)[0] == substring
     ]
+
 
 def col_with_substr_q(df, substring):
     """
@@ -25,6 +29,7 @@ def col_with_substr_q(df, substring):
         if (": " in col) and (col.split(": ", 1)[1] == substring)
     ]
 
+
 def col_with_substr_a(df, substring, qid):
     """
     Another helper function for checkbox questions.
@@ -36,14 +41,15 @@ def col_with_substr_a(df, substring, qid):
         if qid in col and substring in col
     ]
 
+
 def col_with_qid(df, qid):
     """
     Returns a dataframe with all the columns
     that contain a question ID
     """
-    return [
-        col for col in df.columns if f"{qid}:" in col
-    ]
+    pattern = f"^{qid}:"
+    return [col for col in df.columns if re.match(pattern, col)]
+
 
 def col_substr_partial(df, substring):
     """
