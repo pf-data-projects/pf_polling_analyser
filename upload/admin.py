4d8683cc63c30455f520435c5be09ccccfb8f36a
Jeremy Simons <jeremy.simons@publicfirst.co.uk>
2024-08-06 10:18:15 +0100

    FEAT: register crossbreaks to admin

--- upload/admin.py
+++ upload/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Crossbreak
 
-# Register your models here.
+
+@admin.register(Crossbreak)
+class CrossbreakAdmin(admin.ModelAdmin):
+    """
+    A class to register the crossbreak instances
+    in the admin panel.
+    """
+    list_display = ('user', 'created_at', 'updated_at')
+    search_fields = ('user__username',)
